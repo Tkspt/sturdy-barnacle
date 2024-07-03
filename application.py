0943a9a0939a6ce5c3cfe35dd4fc05a99c91025a
Mor Codou Seck <morcodou.seck@ldb.lan>
2024-07-03 18:49:01 +0000

    first update of chebyshev_distance function

--- application.py
+++ application.py
@@ -1,6 +1,12 @@
 def chebyshev_distance(p1, p2):
     """Docstring..."""
-    return max(abs(x - y) for x, y in zip(p1, p2))
+    # return max(abs(x - y) for x, y in zip(p1, p2))
+    max_distance = 0
+    for x, y in zip(p1, p2):
+        distance = abs(x - y)
+        if distance > max_distance:
+            max_distance = distance
+    return max_distance
 
 if __name__ == "__main__":
     point1 = (1, 2, 3)
